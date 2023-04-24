1000e5e4b2483cc423dcf850b41753be07bca093
Akshay Prabhu <12824090+akshayprabhu200@users.noreply.github.com>
2023-04-24 16:26:07 -0700

    Added accelerometer

---
+++ js/accelerometer.js
@@ -0,0 +1,24 @@
+export default function getAccelerometerData() {
+  if (
+    typeof DeviceMotionEvent !== "undefined" &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    // The Permissions API is supported
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          // The permission has been granted
+          window.addEventListener("devicemotion", (e) => {
+            const { x, y, z } = e.acceleration;
+            console.log(`X: ${x}, Y: ${y}, Z: ${z}`);
+            // Use the x, y, and z values as needed
+          });
+        } else {
+          console.log("Permission for accelerometer access denied");
+        }
+      })
+      .catch(console.error);
+  } else {
+    console.log("Accelerometer is not supported on this device");
+  }
+}
