802022638ea7e9069ded1d1df49d780b45ae8f06
Akshay Prabhu <12824090+akshayprabhu200@users.noreply.github.com>
2023-04-27 15:45:41 -0700

    Changed cache location

--- cache.js
+++ cache.js
@@ -8,12 +8,12 @@ self.addEventListener("install", (event) => {
     caches.open(cacheName).then((cache) => {
       return cache.addAll([
         "/",
-        "/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
-        "/android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 0.75 screen density
+        "android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
+        "android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 0.75 screen density
         "/js/accelerometer.js",
         "/js/gps.js",
         "/js/passcode_handler.js",
-        "/manifest.json",
+        "manifest.json",
       ]);
     })
   );
