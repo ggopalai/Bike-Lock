ce67b924cc1804fdc094e5a61c6a9a109f9cf91e
Gagan Gowda <gagangowda26@gmail.com>
2023-05-21 14:46:56 -0700

    refactored startAccelerometer()

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -39,101 +39,117 @@ export default function getAccelerometerData() {
 function startAccelerometer() {
   // Check for support for the Accelerometer API
   if ("Accelerometer" in window) {
-    
-    var lat, long;
     getLocation()
-    .then(location => {
-      lat = location.latitude;
-      long = location.longitude;
-      console.log(location.latitude, location.longitude);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-    
-    // Create an accelerometer object
-    var accelerometer = new Accelerometer({ frequency: 60 });
-    var lastX, lastY, lastZ;
-    accelerometer.addEventListener("reading", function () {
-      // Calculate the difference between the current and last accelerometer readings
-      var deltaX = Math.abs(lastX - accelerometer.x);
-      var deltaY = Math.abs(lastY - accelerometer.y);
-      var deltaZ = Math.abs(lastZ - accelerometer.z);
-      // Check if the difference exceeds the threshold
-      if (deltaX > threshold || deltaY > threshold || deltaZ > threshold) {
-        const recipient = localStorage.getItem('email');
-        console.log(recipient);
-
-        document.getElementById("accelerometer-data").textContent =
-          "Movement detected! - X: " +
-          accelerometer.x.toFixed(2) +
-          ", Y: " +
-          accelerometer.y.toFixed(2) +
-          ", Z: " +
-          accelerometer.z.toFixed(2);
-
-          var audio = new Audio('./sounds/ucsdfight.mp3');
-          var loopCount = 0;
-          var isPlaying = false;
-          
-          audio.addEventListener('ended', function() {
-            if (!isPlaying) {
-              loopCount++;
-              if (loopCount < 5) {
-                isPlaying = true;
-                audio.play();
-              }
-            }
-          });
-          
-          audio.addEventListener('play', function() {
-            isPlaying = true;
-          });
-          
-          audio.addEventListener('pause', function() {
-            isPlaying = false;
-          });
-          
-          audio.play();
-          
-          
-        
-        // Send POST request to endpoint
-        fetch('https://y0d50hlxmi.execute-api.us-west-1.amazonaws.com/beta/email', {
-          method: 'POST',
-          mode: 'no-cors',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "subject": "ALERT!!!!",
-            "message": "YOUR BIKE IS BEING STOLEN!!!!!",
-            "recipient": recipient,
-            "gps_lat": lat,
-            "gps_long": long
-          })
-        })
-        .then(response => response.json())
-        .then(data => function () {
-          console.log(data);
-          document.getElementById("sandbox").textContent = data;
-        })
-        .catch(error => function () {
-          console.error(error)
-          document.getElementById("sandbox").textContent = data;
-        });
-      } else {
-        document.getElementById("accelerometer-data").textContent =
-          "No movement detected.";
-      }
-      // Update the last accelerometer readings
-      lastX = accelerometer.x;
-      lastY = accelerometer.y;
-      lastZ = accelerometer.z;
-    });
-    accelerometer.start();
+      .then(location => {
+        var lat = location.latitude;
+        var long = location.longitude;
+        console.log(location.latitude, location.longitude);
+        createAccelerometer(lat, long);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   } else {
     // Accelerometer API not supported
     document.getElementById("accelerometer-data").textContent = "The Accelerometer API is not supported in this browser.";
   }
+}
+
+function createAccelerometer(lat, long) {
+  // Create an accelerometer object
+  var accelerometer = new Accelerometer({ frequency: 60 });
+  var lastX, lastY, lastZ;
+
+  accelerometer.addEventListener("reading", function () {
+    detectMovement(accelerometer, lat, long, lastX, lastY, lastZ);
+    updateLastReadings(accelerometer, lastX, lastY, lastZ);
+  });
+
+  accelerometer.start();
+}
+
+function detectMovement(accelerometer, lat, long, lastX, lastY, lastZ) {
+  var deltaX = Math.abs(lastX - accelerometer.x);
+  var deltaY = Math.abs(lastY - accelerometer.y);
+  var deltaZ = Math.abs(lastZ - accelerometer.z);
+
+  if (deltaX > threshold || deltaY > threshold || deltaZ > threshold) {
+    const recipient = localStorage.getItem('email');
+    console.log(recipient);
+
+    document.getElementById("accelerometer-data").textContent =
+      "Movement detected! - X: " +
+      accelerometer.x.toFixed(2) +
+      ", Y: " +
+      accelerometer.y.toFixed(2) +
+      ", Z: " +
+      accelerometer.z.toFixed(2);
+
+    playAudio('./sounds/ucsdfight.mp3');
+
+    sendEmailToEndpoint(recipient, lat, long);
+  } else {
+    document.getElementById("accelerometer-data").textContent =
+      "No movement detected.";
+  }
+}
+
+function updateLastReadings(accelerometer, lastX, lastY, lastZ) {
+  lastX = accelerometer.x;
+  lastY = accelerometer.y;
+  lastZ = accelerometer.z;
+}
+
+function playAudio(audioFile) {
+  var audio = new Audio(audioFile);
+  var loopCount = 0;
+  var isPlaying = false;
+
+  audio.addEventListener('ended', function () {
+    if (!isPlaying) {
+      loopCount++;
+      if (loopCount < 5) {
+        isPlaying = true;
+        audio.play();
+      }
+    }
+  });
+
+  audio.addEventListener('play', function () {
+    isPlaying = true;
+  });
+
+  audio.addEventListener('pause', function () {
+    isPlaying = false;
+  });
+
+  audio.play();
+}
+
+function sendEmailToEndpoint(recipient, lat, long) {
+  const emailParams = {
+    "subject": "ALERT!!!!",
+    "message": "YOUR BIKE IS BEING STOLEN!!!!!",
+    "recipient": recipient,
+    "gps_lat": lat,
+    "gps_long": long
+  };
+
+  fetch('https://y0d50hlxmi.execute-api.us-west-1.amazonaws.com/beta/email', {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(emailParams)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      document.getElementById("sandbox").textContent = data;
+    })
+    .catch(error => {
+      console.error(error);
+      document.getElementById("sandbox").textContent = data;
+    });
 }
