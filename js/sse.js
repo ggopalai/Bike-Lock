ec6f4dc8312b0f604115db18934d63289492e8c3
grishmagurbani <grishmagurbani1998@gmail.com>
2023-05-23 03:16:19 -0700

    reload on get

--- js/sse.js
+++ js/sse.js
@@ -1,4 +1,6 @@
 let email = localStorage.getItem('email');
+const postregLockedDiv = document.getElementById('postreg-locked');
+const displayStatus = document.getElementById('status');
 const url = "http://localhost:3000/api?email=" + email;
 const evtSource = new EventSource(
   url
@@ -8,10 +10,12 @@ evtSource.onmessage = (event) => {
   let newState = JSON.parse(event.data)[email];
   if (newState=="false") {
     localStorage.setItem("state", 0);
+    displayStatus.textContent = 'The device is unlocked.';
+    postregLockedDiv.style.display = 'none';
   }
   else if (newState=="true") {
     localStorage.setItem("state", 1);
+    displayStatus.textContent = 'The device is locked.';
+    postregLockedDiv.style.display = 'block';
   }
-  toggleDiv();
-  window.location.href = window.location.href;
 };
